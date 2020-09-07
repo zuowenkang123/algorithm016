7583610cff0008ff4d4824196fc50bf0dd32ea35
zuowenkang <zuowenkang@luojilab.com>
2020-09-07 15:29:20 +0800

    增加递推逻辑

--- Week_01/climbStairs.go
+++ Week_01/climbStairs.go
@@ -2,6 +2,7 @@ package Week_01
 
 var stairsMap map[int]int
 
+// 递归解法
 func climbStairs(n int) int {
 	// 初始数据
 	if n == 0 || n == 1 || n == 2 {
@@ -17,4 +18,18 @@ func climbStairs(n int) int {
 	}
 
 	return stairsMap[n]
+}
+
+// 动态规划,递推逻辑
+func climbStairsDp(n int) int {
+	stairArr := make([]int, 0, n)
+	// 初始数据
+	stairArr[0] = 0
+	stairArr[1] = 1
+	// 不存在则赋值
+	for i := 2; i < n; i++ {
+		stairArr[i] = stairArr[i-1] + stairArr[i-2]
+	}
+
+	return stairArr[n]
 }
---
+++ Week_01/climbStairs_test.go
@@ -0,0 +1,9 @@
+package Week_01
+
+import (
+	"testing"
+)
+
+func TestClimbStairsDp(t *testing.T) {
+	climbStairsDp(10)
+}
