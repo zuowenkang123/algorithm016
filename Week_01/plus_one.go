489e8c9f95c0995881410c465744d41fd0933749
zuowenkang <zuowenkang@luojilab.com>
2020-09-08 11:04:25 +0800

    简化代码

--- Week_01/plus_one.go
+++ Week_01/plus_one.go
@@ -2,6 +2,22 @@ package Week_01
 
 // 暴力法，最后处理进位的问题
 func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i]++
+		digits[i] = digits[i] % 10
+		if digits[i] != 0 {
+			return digits
+		}
+	}
+
+	res := make([]int, 0, len(digits)+1)
+	res = append(res, 1)
+	res = append(res, digits...)
+	return res
+}
+
+// 进位可见
+func plusOne1(digits []int) []int {
 	carry := 1
 	for i := len(digits) - 1; i >= 0; i-- {
 		carry = (digits[i] + carry) / 10
@@ -10,10 +26,12 @@ func plusOne(digits []int) []int {
 			break
 		}
 	}
-	res := make([]int, 0, len(digits))
-	if carry == 1 {
-		res = append(res, carry)
+
+	if carry == 0 {
+		return digits
 	}
+	res := make([]int, 0, len(digits))
+	res = append(res, carry)
 	for _, val := range digits {
 		res = append(res, val)
 	}
--- Week_01/plus_one_test.go
+++ Week_01/plus_one_test.go
@@ -3,5 +3,5 @@ package Week_01
 import "testing"
 
 func TestPlusOne(t *testing.T) {
-	plusOne([]int{1, 9, 9, 9})
+	plusOne([]int{1, 9, 9, 8})
 }
