31b1340392b1439f654b334d02083b08b0bb8b5d
zuowenkang <zuowenkang@luojilab.com>
2020-09-09 11:05:23 +0800

    有序去重代码

--- Week_01/find_repeat_number_test.go
+++ Week_01/find_repeat_number_test.go
@@ -1,5 +1,7 @@
 package Week_01
 
+import "sort"
+
 // https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
 // 2020-09-09
 
@@ -11,6 +13,21 @@ func findRepeatNumber(nums []int) int {
 			return num
 		}
 		numMap[num] = true
+	}
+	return 0
+}
+
+// 有序的去重,后面对比前面
+func findRepeatNumber1(nums []int) int {
+	if len(nums) < 2 {
+		return 0
+	}
+	sort.Ints(nums)
+
+	for i := 1; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			return nums[i]
+		}
 	}
 	return 0
 }
