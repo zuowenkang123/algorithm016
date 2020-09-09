087bfa80533ff4cccb7e89299cf4454b8c928d37
zuowenkang <zuowenkang@luojilab.com>
2020-09-09 10:59:17 +0800

    map是常用的去重

--- Week_01/find_repeat_number_test.go
+++ Week_01/find_repeat_number_test.go
@@ -3,6 +3,14 @@ package Week_01
 // https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
 // 2020-09-09
 
+// map是常用的去重
 func findRepeatNumber(nums []int) int {
+	numMap := make(map[int]bool)
+	for _, num := range nums {
+		if _, ok := numMap[num]; ok {
+			return num
+		}
+		numMap[num] = true
+	}
 	return 0
 }
