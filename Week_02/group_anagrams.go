5dad31699da409568a531df9a8249c7de019f1d9
zuowenkang <zuowenkang@luojilab.com>
2020-09-16 17:45:43 +0800

    26数组法

--- Week_02/group_anagrams.go
+++ Week_02/group_anagrams.go
@@ -1,6 +1,9 @@
 package Week_02
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 // https://leetcode-cn.com/problems/group-anagrams/
 // 2020-09-15
@@ -14,9 +17,7 @@ func groupAnagrams(strs []string) [][]string {
 		sort.Sort(charSlc)
 		_, ok := strMap[string(charSlc)]
 		if !ok {
-			strArr := make([]string, 0)
-			strArr = append(strArr, str)
-			strMap[string(charSlc)] = strArr
+			strMap[string(charSlc)] = []string{str}
 		} else {
 			strMap[string(charSlc)] = append(strMap[string(charSlc)], str)
 		}
@@ -26,4 +27,34 @@ func groupAnagrams(strs []string) [][]string {
 		res = append(res, strArr)
 	}
 	return res
+}
+
+// 字母累加法
+func groupAnagrams1(strs []string) [][]string {
+	strMap := make(map[string][]string)
+	for _, str := range strs {
+		keyArr := make([]int, 26, 26)
+		for i := 0; i < len(str); i++ {
+			keyArr[rune(str[i])-'a']++
+		}
+		keyStr := ""
+		for _, key := range keyArr {
+			keyStr = fmt.Sprintf("%s%d#", keyStr, key)
+		}
+		if _, ok := strMap[keyStr]; !ok {
+			strMap[keyStr] = []string{str}
+		} else {
+			strMap[keyStr] = append(strMap[keyStr], str)
+		}
+	}
+	res := make([][]string, 0)
+	for _, strArr := range strMap {
+		res = append(res, strArr)
+	}
+	return res
+}
+
+// 质数法
+func groupAnagrams2(strs []string) [][]string {
+	return nil
 }
