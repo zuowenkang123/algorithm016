2c163e13791cc76685f8ba1ea078af06c91327cc
zuowenkang <zuowenkang@luojilab.com>
2020-09-16 14:29:00 +0800

    排序map法

--- Week_02/group_anagrams.go
+++ Week_02/group_anagrams.go
@@ -1,8 +1,29 @@
 package Week_02
 
+import "sort"
+
 // https://leetcode-cn.com/problems/group-anagrams/
 // 2020-09-15
 
+// 排序map法
 func groupAnagrams(strs []string) [][]string {
-
+	strMap := make(map[string][]string)
+	for _, str := range strs {
+		charArr := []byte(str)
+		charSlc := CharSlice(charArr)
+		sort.Sort(charSlc)
+		_, ok := strMap[string(charSlc)]
+		if !ok {
+			strArr := make([]string, 0)
+			strArr = append(strArr, str)
+			strMap[string(charSlc)] = strArr
+		} else {
+			strMap[string(charSlc)] = append(strMap[string(charSlc)], str)
+		}
+	}
+	res := make([][]string, 0)
+	for _, strArr := range strMap {
+		res = append(res, strArr)
+	}
+	return res
 }
--- Week_02/util.go
+++ Week_02/util.go
@@ -13,3 +13,22 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+// 字符排序用
+type CharSlice []byte
+
+func (this CharSlice) Len() int {
+	return len(this)
+}
+
+func (this CharSlice) Less(i, j int) bool {
+	if this[i] > this[j] {
+		return false
+	}
+	return true
+}
+
+func (this CharSlice) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+	return
+}
