f52852e1ccd3261e898a5e7a55f0bd3d759383d0
zuowenkang <zuowenkang@luojilab.com>
2020-09-18 16:28:53 +0800

    模拟方式和数学方式

--- Week_02/add_digits.go
+++ Week_02/add_digits.go
@@ -12,3 +12,16 @@ func addDigits(num int) int {
 func addDigits1(num int) int {
 	return (num-1)%9 + 1
 }
+
+func addDigits2(num int) int {
+	if num < 10 {
+		return num
+	}
+	// 计算当前的余数和
+	next := 0
+	for num != 0 {
+		next = next + num%10
+		num /= 10
+	}
+	return addDigits2(next)
+}
---
+++ Week_02/add_digits_test.go
@@ -0,0 +1,9 @@
+package Week_02
+
+import (
+	"testing"
+)
+
+func TestAddDigits(t *testing.T) {
+	addDigits(111)
+}
