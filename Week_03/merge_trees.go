199f71493de9db417d8658336b0332a875798594
zuowenkang <zuowenkang@luojilab.com>
2020-09-23 19:35:10 +0800

    深度优先和广度优先

--- Week_03/merge_trees.go
+++ Week_03/merge_trees.go
@@ -4,6 +4,7 @@ package Week_03
 // 2020-09-23
 
 // 递归，当前即是所有。当前判断t1、t2为空，即所有逻辑都是，当前赋值左右节点，即所有都是。
+// dfs
 func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 	if t1 == nil {
 		return t2
@@ -33,4 +34,42 @@ func mergeTrees1(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 		mergeTrees(t1.Right, t2.Right)
 	}
 	return root
+}
+
+// bfs
+func mergeTrees2(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	queue := TreeNodeQueue{}
+	queue.push(t1)
+	queue.push(t2)
+	for queue.size() > 0 {
+		node1 := queue.pop()
+		node2 := queue.pop()
+		// 计算逻辑
+		node1.Val += node2.Val
+		if node1.Left == nil {
+			// 追加逻辑
+			node1.Left = node2.Left
+		} else {
+			if node2.Left != nil {
+				queue.push(node1.Left)
+				queue.push(node2.Left)
+			}
+		}
+		if node1.Right == nil {
+			// 追加逻辑
+			node1.Right = node2.Right
+		} else {
+			if node2.Right != nil {
+				queue.push(node1.Right)
+				queue.push(node2.Right)
+			}
+		}
+	}
+	return t1
 }
