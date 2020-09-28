627ed7545defe90c958a5be086fed90e229053e4
zuowenkang123 <812092141@qq.com>
2020-09-28 21:11:14 +0800

    常用的树的行处理办法

---
+++ Week_04/largest_values.go
@@ -0,0 +1,34 @@
+package Week_04
+
+import "math"
+
+// https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/
+// 2020-09-28
+
+//
+func largestValues(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+	queue := TreeNodeQueue{}
+	queue.push(root)
+	for queue.size() > 0 {
+		size := queue.size()
+		maxInt := math.MinInt32
+		for size > 0 {
+			node := queue.pop()
+			size--
+			maxInt = max(maxInt, node.Val)
+			if node.Left != nil {
+				queue.push(node.Left)
+			}
+			if node.Right != nil {
+				queue.push(node.Right)
+			}
+
+		}
+		res = append(res, maxInt)
+	}
+	return res
+}
---
+++ Week_04/tree_node.go
@@ -0,0 +1,7 @@
+package Week_04
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
