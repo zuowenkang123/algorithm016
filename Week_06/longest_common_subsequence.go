28a348b6109bc9385f34fe76853457ec18c9b970
zuowenkang123 <812092141@qq.com>
2020-10-09 22:35:14 +0800

    递推dp

---
+++ Week_06/longest_common_subsequence.go
@@ -0,0 +1,21 @@
+package Week_06
+
+// https://leetcode-cn.com/problems/longest-common-subsequence/
+// 2020-10-09 22:23:56
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i <= len(text1); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[len(text1)][len(text2)]
+}
