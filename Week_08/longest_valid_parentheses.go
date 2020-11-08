3e27288090141bdd9bd16da53640fdfbddbc49f8
zuowenkang123 <812092141@qq.com>
2020-11-08 20:52:53 +0800

    最长括号

---
+++ Week_08/longest_valid_parentheses.go
@@ -0,0 +1,23 @@
+package Week_08
+
+// https://leetcode-cn.com/problems/longest-valid-parentheses/submissions/
+// 2020-11-08
+
+func longestValidParentheses(s string) int {
+	maxAns := 0
+	stack := []int{}
+	stack = append(stack, -1)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+		} else {
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				maxAns = max(maxAns, i-stack[len(stack)-1])
+			}
+		}
+	}
+	return maxAns
+}
