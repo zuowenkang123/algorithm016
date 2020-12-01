9e4fd14312cbefeb640c4399b759c37f00e9704f
kangyou21 <812092141@qq.com>
2020-12-01 16:55:41 +0800

    解码方法

--- Week_09/num_decodings.go
+++ Week_09/num_decodings.go
@@ -11,16 +11,42 @@ func numDecodings(s string) int {
 	cur, pre := 1, 1
 	for i := 1; i < len(s); i++ {
 		tmp := cur
+		// 10/20的情况
 		if s[i] == '0' {
 			if s[i-1] != '1' && s[i-1] != '2' {
 				return 0
 			} else {
 				cur = pre
 			}
-		} else if s[i-1] == '1' || (s[i-1] == '2' && s[i] <= '6') {
+		} else if s[i-1] == '1' || (s[i-1] == '2' && s[i] <= '6') { // 1x/2x的情况
 			cur += pre
 		}
-		pre = tmp
+		pre = tmp // 个位的情况
 	}
 	return cur
+}
+
+func numDecodings1(s string) int {
+	if len(s) == 0 || s[0] == '0' {
+		return 0
+	}
+
+	dp := make([]int, len(s)+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 1; i < len(s); i++ {
+		// 10/20的情况
+		if s[i] == '0' {
+			if s[i-1] != '1' && s[i-1] != '2' {
+				return 0
+			} else {
+				dp[i+1] = dp[i-1]
+			}
+		} else if s[i-1] == '1' || (s[i-1] == '2' && s[i] <= '6') { // 1x/2x的情况
+			dp[i+1] = dp[i] + dp[i-1]
+		} else {
+			dp[i+1] = dp[i] // 个位的情况
+		}
+	}
+	return dp[len(s)]
 }
