0a838608e8451339f649e6d323754320c13189b1
kangyou21 <812092141@qq.com>
2020-12-04 15:51:07 +0800

    dp:n

--- Week_01/climb_stairs.go
+++ Week_01/climb_stairs.go
@@ -54,3 +54,20 @@ func climbStairsDp2(n int) int {
 
 	return result
 }
+
+// 动态规划,递推解法，数组填入法
+func climbStairsDp3(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	dp := make([]int, n+1)
+	// 初始数据
+	dp[1] = 1
+	dp[2] = 2
+	// 不存在则赋值
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
